356a1d32abcd9af3ee986a3eb00e9959eecf2abc
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-06-15 13:44:12 +0200

    add repo exercise

---
+++ src/main/java/com/exercise/JAVAU2W3D4/JavaU2W3D4Application.java
@@ -0,0 +1,13 @@
+package com.exercise.JAVAU2W3D4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaU2W3D4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaU2W3D4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/JAVAU2W3D4/JavaU2W3D4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.JAVAU2W3D4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaU2W3D4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
